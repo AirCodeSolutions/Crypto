ec5a59ed76ac8a7d4d08e31e3786c7dcd7dc0ec4
AirCodeSolutions <belughanocode@gmail.com>
2024-11-29 20:05:29 +0100

    ia

--- interface.py
+++ interface.py
@@ -12,7 +12,7 @@ from utils import (
 )
 from technical_analysis import SignalGenerator, TechnicalAnalysis  # Ajout de TechnicalAnalysis
 from portfolio_management import PortfolioManager  # Ajout de cet import
-
+from ai_predictor import AIPredictor, AITester  # Ajout de ces imports
 
 
 class LiveAnalysisPage:
