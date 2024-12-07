89df27fdfebef0458b86fe3aabc37145a6ce8d29
AirCodeSolutions <belughanocode@gmail.com>
2024-12-07 09:11:35 +0000

    modif import

--- interface/__init__.py
+++ interface/__init__.py
@@ -1,12 +1,14 @@
 # interface/__init__.py
 from .components.widgets import TimeSelector
 from .components.guide_helper import GuideHelper
+from .components.chart_components import TradingChart, ChartConfig
 from .pages.live_analysis import LiveAnalysisPage
 from .pages.top_performance import TopPerformancePage
 
 # Définissons explicitement ce qui est disponible
 __all__ = [
     'TimeSelector',
+    'TradingChart',
     'GuideHelper',
     'LiveAnalysisPage',
     'TopPerformancePage'
