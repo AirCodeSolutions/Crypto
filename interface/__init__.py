272315584441500d64fd3954435e512c400e9b3d
AirCodeSolutions <belughanocode@gmail.com>
2024-12-07 09:17:01 +0000

    alerte

--- interface/__init__.py
+++ interface/__init__.py
@@ -4,6 +4,7 @@ from .components.guide_helper import GuideHelper
 from .components.chart_components import TradingChart, ChartConfig
 from .pages.live_analysis import LiveAnalysisPage
 from .pages.top_performance import TopPerformancePage
+from .components.alerts import AlertSystem
 
 # Définissons explicitement ce qui est disponible
 __all__ = [
@@ -11,5 +12,6 @@ __all__ = [
     'TradingChart',
     'GuideHelper',
     'LiveAnalysisPage',
+    'AlertSystem',
     'TopPerformancePage'
 ]
