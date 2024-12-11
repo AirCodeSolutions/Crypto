d4ad15b358ff955e505d7149100c07d70ee7904a
AirCodeSolutions <belughanocode@gmail.com>
2024-12-11 18:25:38 +0100

    Delete core directory

--- core/__init__.py
+++
@@ -1,8 +0,0 @@
-# core/__init__.py
-from .analysis import TechnicalIndicators, TradingSignalAnalyzer, MarketAnalyzer
-
-__all__ = [
-    'TechnicalIndicators',
-    'TradingSignalAnalyzer',
-    'MarketAnalyzer'
-]
