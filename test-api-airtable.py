0f657d7b1721bb17bc4c62aa04a5d9776a798bf6
AirCodeSolutions <belughanocode@gmail.com>
2024-12-12 18:08:37 +0000

    test

--- test-api-airtable.py
+++ test-api-airtable.py
@@ -1,13 +1,8 @@
 import streamlit as st
 from pyairtable import Api
 from pyairtable.api.table import Table
-import streamlit as st
-import time
-import pandas as pd
-from datetime import datetime
-import logging
+from pyairtable import Base
 
-# Clés et configurations (à mettre dans .streamlit/secrets.toml sur Streamlit Cloud)
 api_key = st.secrets["api_key"]
 base_id = "appwYozXBGggzUjCW"
 table_id = "tblEthZxlqwvYqK3R"
@@ -18,8 +13,11 @@ try:
     # Création de l'API
     api = Api(api_key)
     
+    # Création de la Base
+    base = Base(api, base_id)
+    
     # Création de la table
-    table = Table(api, base_id, table_id)
+    table = base.table(table_id)
     
     # Test de récupération des données
     records = table.all()
@@ -27,7 +25,6 @@ try:
     st.success("✅ Connexion réussie à Airtable!")
     st.write(f"Nombre d'enregistrements trouvés : {len(records)}")
     
-    # Affichage des enregistrements
     if records:
         st.write("Premier enregistrement :")
         st.write(records[0])
